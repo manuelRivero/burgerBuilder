f2dfd8a0e4870f219728f514ff14f0c50cf47542
manuel rivero <manuel.enrique.r.v@gmail.com>
2019-12-01 13:45:02 -0300

    add spinner for http request

---
+++ src/components/UI/spinner/spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./spinner.module.css"
+
+export default function spinner() {
+  return (
+    <>
+    <div className={styles.Loader}>Loading...</div>
+    </>
+  );
+}
