7a55bbf091c4e4dae29a74174a84b5a6dfbfbb57
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-03 22:49:14 -0300

    Updates

---
+++ precache-manifest.79a3814a7752c6a2bf1717295945b90f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d65037a21bfe82c7cbe4cb777728cb6",
+    "url": "/burgerBuilder/index.html"
+  },
+  {
+    "revision": "56ada54f9512521703ad",
+    "url": "/burgerBuilder/static/css/main.acfabe1d.chunk.css"
+  },
+  {
+    "revision": "c1b52c33b2ede4effdc4",
+    "url": "/burgerBuilder/static/js/2.f0b0f3f9.chunk.js"
+  },
+  {
+    "revision": "56ada54f9512521703ad",
+    "url": "/burgerBuilder/static/js/main.a73da60f.chunk.js"
+  },
+  {
+    "revision": "91645afdc1bfa8e255ee",
+    "url": "/burgerBuilder/static/js/runtime-main.f84d6408.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerBuilder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/burgerBuilder/static/media/logo.25bf045c.svg"
+  }
+]);
