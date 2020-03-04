335500cf3bf0ae049b1eef3131650520c3ff214e
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-03 23:32:38 -0300

    [add] check auth state in app component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import  styles from './App.module.css';
 
-import {Route} from 'react-router-dom'
+import {Route, withRouter} from 'react-router-dom'
+
+import {connect} from 'react-redux';
+import * as actions from './redux/actions';
 
 import Layout from './components/layout/loyout';
 import BurgerBuilder from './containers/burgerBuilder/burgerBuilder';
 import Checkout from './containers/checkout/checkout'
 import Order from './containers/orders/orders';
 import Auth from './containers/auth/auth'
+import Logout from './containers/auth/logout/logout';
 
-function App() {
+function App(props) {
+   props.onCheckAuthState();
+
   return (
     <Layout>      
       <Route path='/checkout' component={Checkout}/> 
       <Route path='/orders' component={Order} />
       <Route path='/auth'  exact component={Auth}/>
+      <Route path='/logout' exact component={Logout} />
       <Route path='/'  exact component={BurgerBuilder}/>
     </Layout>
   );
 }
 
-export default App;
+const mapDispatchToProps = dispatch =>{
+  return {
+    onCheckAuthState : () => {dispatch(actions.authState())}
+  }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(App))
