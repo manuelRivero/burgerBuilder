1a34628d1082fb3014a12d1c97c6c45be6cda3be
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-18 12:41:45 -0300

    [add] spinner in log out

--- src/containers/auth/logout/logout.js
+++ src/containers/auth/logout/logout.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import {Redirect, withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
-import * as actions from './../../../redux/actions';
-import Spinner from '../../../components/UI/spinner/spinner';
+import React from "react";
+import { Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "./../../../redux/actions";
+import Spinner from "../../../components/UI/spinner/spinner";
 
 function Logout(props) {
-    let logOut = <Spinner />
+  let logOut = <Spinner />;
 
-    if(props.isAuth ){
-        props.onLogout()
-        
-    }else{
-        logOut = <Redirect to="/" />
-    }
-    
-  return (
-    <>
-    {logOut}
-    </>
-  );
+  if (props.isAuth) {
+    setTimeout(() => {
+      props.onLogout();
+    }, 500);
+  } else {
+    logOut = <Redirect to="/" />;
+  }
+
+  return <>{logOut}</>;
 }
 
-const mapDsipatchToprops = dispatch =>{
-    return{
-        onLogout : () => { dispatch(actions.logOut())}
+const mapDsipatchToprops = dispatch => {
+  return {
+    onLogout: () => {
+      dispatch(actions.logOut());
     }
-}
+  };
+};
 
 const mapStateToProps = state => {
-    return {
-        isLoading: state.auth.loading,
-        error: state.auth.error,
-        isAuth: state.auth.user.tokenId !== null
-    }
-}
+  return {
+    isLoading: state.auth.loading,
+    error: state.auth.error,
+    isAuth: state.auth.user.tokenId !== null
+  };
+};
 
 export default withRouter(connect(mapStateToProps, mapDsipatchToprops)(Logout));
