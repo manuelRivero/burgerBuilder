b6fb88558adffe35608fb4efe453bdfddcba0d2f
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-03-23 12:28:17 -0300

    [css][fix] burger layout in checkout view

--- src/components/checkoutSummary/checkoutSummary.js
+++ src/components/checkoutSummary/checkoutSummary.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import Burger from './../burger/burger';
-import Buttom from './../UI/button/button';
+import React from "react";
+import Burger from "./../burger/burger";
+import Buttom from "./../UI/button/button";
 
-import styles from './checkoutSummary.module.css';
+import styles from "./checkoutSummary.module.css";
 
 export default function checkoutSummary(props) {
   return (
     <>
-        <div className={styles.CheckoutSummary}>
-            <h1> Enjoy it !</h1>
-            <div style={{width:'300px', margin:'auto'}}>
-                <Burger ingredient={props.ingredient}/>
-            </div>
-            
-            <Buttom clicked={props.checkoutCancelled} type="Danger">Cancel</Buttom>
-            <Buttom clicked={props.checkoutContinue} type="Success">Contiue</Buttom>
+      <div className={styles.CheckoutSummary}>
+        <h1> Enjoy it !</h1>
+        <Burger ingredient={props.ingredient} />
 
-        </div>
+        <Buttom clicked={props.checkoutCancelled} type="Danger">
+          Cancel
+        </Buttom>
+        <Buttom clicked={props.checkoutContinue} type="Success">
+          Contiue
+        </Buttom>
+      </div>
     </>
   );
 }
